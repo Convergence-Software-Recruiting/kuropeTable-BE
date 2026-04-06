ec4c458ed5cb87ee050894afd4ad1fff7a2ecd18
hughryu1125 <hughr1125@gmail.com>
2026-04-07 02:54:48 +0900

    chore: 백엔드 초기 개발 환경 설정

---
+++ src/main/java/com/example/kurope/KuropeApplication.java
@@ -0,0 +1,14 @@
+package com.example.kurope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class KuropeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuropeApplication.class, args);
+	}
+
+}
